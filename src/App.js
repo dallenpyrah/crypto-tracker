f1fd665e279997e95d246b68c6358a706fbdf428
Dallen Pyrah <dallenpyrah@gmail.com>
2021-05-06 14:59:03 -0600

    Finished

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Col, Container, Form, Row } from 'react-bootstrap';
 import './App.css';
 import Coin from './Coin';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
@@ -24,11 +25,14 @@ function App() {
     coin.name.toLowerCase().includes(search.toLowerCase()));
 
   return (
-    <Container>
-      <Row>
-        <Col><Form><input className="form-control" onChange={handleChange} placeholder="Search" ></input></Form></Col>
+    <Container fluid>
+      <Row className="justify-content-center mt-5">
+        <Col className="text-center"><h3><b>Search a Currrency</b></h3></Col>
       </Row>
-      <Row>{filteredCoins.map(coin => {
+      <Row className="justify-content-center mt-3 mb-4">
+        <Col className="text-center" xs={4}><Form><input className="form-control p-3 input text-light" onChange={handleChange} placeholder="Search" ></input></Form></Col>
+      </Row>
+      <Row className="justify-content-center">{filteredCoins.map(coin => {
         return (
           <Coin key={coin.id} name={coin.name} symbol={coin.symbol} price={coin.current_price} volume={coin.total_volume} marketcap={coin.market_cap}
           image={coin.image} priceChange={coin.price_change_percentage_24h}/>
--- src/Coin.js
+++ src/Coin.js
@@ -1,6 +1,8 @@
-  
+
 import React from 'react';
+import { Badge } from 'react-bootstrap';
 import './Coin.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Coin = ({
   name,
@@ -12,29 +14,30 @@ const Coin = ({
   priceChange
 }) => {
   return (
-    <div className='coin-container'>
-      <div className='coin-row'>
-        <div className='coin'>
-          <img src={image} alt='crypto' />
-          <h1>{name}</h1>
-          <p className='coin-symbol'>{symbol}</p>
+        <div className="col-9 card m-3 p-3 back-dark rounded">
+            <div className="row mt-3 back-dark text-light">
+                <div className="col-3 back-dark mb-3">
+                    <h5><img className="" alt="" src={image} width="75px" /><span className="ml-5"><b>{name}</b></span></h5>
+                </div>
+                <div className="col-1 back-dark">
+                    <h5 className="back-dark mt-4"><Badge>{symbol.toUpperCase()}</Badge></h5>
+                </div>
+                <div className="col-1 back-dark">
+                    <h5 className="back-dark mt-4"><Badge>${price}</Badge></h5>
+                </div>
+                <div className="col-2 back-dark">
+                    <h5 className="back-dark mt-4"><Badge>${volume.toLocaleString()}</Badge></h5>
+                </div>
+                <div className="col-2 back-dark">
+                    {priceChange > 0 ? (
+                    <h5 className="back-dark text-green mt-4">{priceChange.toFixed(2)}%</h5>
+                    ) : (<h5 className="back-dark mt-4 text-red">{priceChange.toFixed(2)}%</h5>)}
+                </div>
+                <div className="col-2 back-dark">
+                    <h5 className="back-dark mt-4"><Badge>Mk Cap ${marketcap.toLocaleString()}</Badge></h5>
+                </div>
+            </div>
         </div>
-        <div className='coin-data'>
-          <p className='coin-price'>${price}</p>
-          <p className='coin-volume'>${volume.toLocaleString()}</p>
-
-          {priceChange < 0 ? (
-            <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
-          ) : (
-            <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
-          )}
-
-          <p className='coin-marketcap'>
-            Mkt Cap: ${marketcap.toLocaleString()}
-          </p>
-        </div>
-      </div>
-    </div>
   );
 };
 
