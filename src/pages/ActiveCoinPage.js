e463e50e2fd2b20b3a6fb1c7879157f0cc5f48b5
Dallen Pyrah <dallenpyrah@gmail.com>
2021-05-10 11:45:27 -0600

    minor

--- src/pages/ActiveCoinPage.js
+++ src/pages/ActiveCoinPage.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect } from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router'
 import { selectedCoin, removeSelectedCoin} from '../redux/actions/CoinActions'
@@ -25,10 +26,26 @@ const ActiveCoinPage = () => {
         return () => {
           dispatch(removeSelectedCoin());
         };
-      }, [id]);
+      }, []);
     
     return (
-        <div className="text-light">ID: {id} {coin.name}</div>
+        <Container fluid>
+            <Row className="justify-content-center">
+                <Col sm={3}>
+                    <div className="card bg-dark text-center">
+                        <h3 className="bg-dark text-light mt-2">{coin.name}</h3>
+                        <div className="card bg-secondary text-left">
+                            <h6></h6>
+                        </div>
+                    </div>
+                </Col>
+                <Col sm={6}>
+                    <div className="card bg-dark text-center">
+                        <h3 className="bg-dark text-light mt-2">News</h3>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
